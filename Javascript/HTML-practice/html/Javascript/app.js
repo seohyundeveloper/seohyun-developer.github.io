5b9d38fb8c14aa4ffa805654047620eac31e0ca4
MIN SEO HYUN <seoandjisister77@gmail.com>
2022-06-10 01:43:36 +0900

    getElementsByClassName()

--- Javascript/HTML-practice/html/Javascript/app.js
+++ Javascript/HTML-practice/html/Javascript/app.js
@@ -42,24 +42,29 @@ const calculator = {
  
  // console.log(test, t1,t2,t3 )
 
- const age = parseInt ( prompt( "How old are you ?") ) // prompt는 인자를 두개 받는다
- //parseInt를 이용해서 string을 걸러낸다
- console.log(isNaN(age));
+//  const age = parseInt ( prompt( "How old are you ?") ) // prompt는 인자를 두개 받는다
+//  //parseInt를 이용해서 string을 걸러낸다
+//  console.log(isNaN(age));
 
- if(isNaN(age) || age < 0){  //숫자 아니야
-    console.log("please write a real positive number");
- } else if( age < 18 ){
-     console.log("you are too young");
- } else if( age >=18 && age <= 50) {
-     console.log("You can drink");
- } else if( age > 50 && age <= 80 ){
-     console.log("you should exercise.");
- } else if( age > 80 ){
-     console.log("you can do whatever you want.");
- }
- 
- 
- 
-//  else{
+//  if(isNaN(age) || age < 0){  //숫자 아니야
+//     console.log("please write a real positive number");
+//  } else if( age < 18 ){
+//      console.log("you are too young");
+//  } else if( age >=18 && age <= 50) {
+//      console.log("You can drink");
+//  } else if( age > 50 && age <= 80 ){
+//      console.log("you should exercise.");
+//  } else if( age > 80 ){
+//      console.log("you can do whatever you want.");
+//  }
+ //  else{
 //      console.log("Thank u for writing your age.")
 //  }
+
+// const title = document.getElementById("title");
+// title.innerText = "Got you!";
+// console.log(title)
+
+const hellos = document.getElementsByClassName("hello");
+ 
+console.log(hellos)  // array가 나온다.
