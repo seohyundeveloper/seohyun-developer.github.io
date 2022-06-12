6a387d3a325c935175364ff4137bc066ed5d44e8
MIN SEO HYUN <seoandjisister77@gmail.com>
2022-06-13 00:53:53 +0900

    input 태그

--- Javascript/HTML-practice/html/Javascript/app2.js
+++ Javascript/HTML-practice/html/Javascript/app2.js
@@ -7,7 +7,15 @@ const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick(){
    // console.dir(loginInput);
-   console.log(loginInput.value);    //input에 입력되는 value 값을 가져올수 있다.
+  // console.log(loginInput.value);    //input에 입력되는 value 값을 가져올수 있다.
+   const username = loginInput.value; 
+
+   //유효성 검사
+   // if(username === '') { 
+   //    alert("Please write your name!!");
+   // } else if(username.length > 15) {
+   //    alert("Your name is too long.");
+   // }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick)
