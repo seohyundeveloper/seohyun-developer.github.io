fb26a576d513aa34534ad5e1b5c397b13b9928d2
MIN SEO HYUN <seoandjisister77@gmail.com>
2022-06-14 01:11:40 +0900

    preventDefault()함수배우기

--- Javascript/HTML-practice/html/Javascript/app2.js
+++ Javascript/HTML-practice/html/Javascript/app2.js
@@ -21,11 +21,21 @@ function onLoginBtnClick(){
    // }
 }
 
-function onLoginSubmit(){ 
+function onLoginSubmit(event){
+   event.preventDefault();
    const username = loginInput.value; 
    console.log(username);
-}
+} 
 
 //loginButton.addEventListener("click", onLoginBtnClick)
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function handleLinkClick(event){
+   event.preventDefault();
+   console.log(event);
+   alert("clicked")  // 기본적으로 자바스크립트의 alert는 기본동작을 잠시 막는 역할을 한다. ok누르면 다시 실행됨!!!
+   
+}
+const link = document.querySelector("a");  //링크찾기
+link.addEventListener("click", handleLinkClick);
