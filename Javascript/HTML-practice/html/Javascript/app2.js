09b32c0c324a0bf6e01f86feff206d4e1be1530a
MIN SEO HYUN <seoandjisister77@gmail.com>
2022-06-14 01:41:59 +0900

    백틱 사용

--- Javascript/HTML-practice/html/Javascript/app2.js
+++ Javascript/HTML-practice/html/Javascript/app2.js
@@ -6,36 +6,45 @@
 // const loginButton = document.querySelector("#login-form button");
 const loginForm = document.querySelector("#login-form"); 
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";  //클래스 이름
 
-
-function onLoginBtnClick(){
-   // console.dir(loginInput);
-  // console.log(loginInput.value);    //input에 입력되는 value 값을 가져올수 있다.
-   const username = loginInput.value; 
-  console.log(username);
-   //유효성 검사
-   // if(username === '') { 
-   //    alert("Please write your name!!");
-   // } else if(username.length > 15) {
-   //    alert("Your name is too long.");
-   // }
-}
+// function onLoginBtnClick(){
+//    // console.dir(loginInput);
+//   // console.log(loginInput.value);    //input에 입력되는 value 값을 가져올수 있다.
+//    const username = loginInput.value; 
+//   console.log(username);
+//    //유효성 검사
+//    // if(username === '') { 
+//    //    alert("Please write your name!!");
+//    // } else if(username.length > 15) {
+//    //    alert("Your name is too long.");
+//    // }
+// }
 
 function onLoginSubmit(event){
    event.preventDefault();
+   loginForm.classList.add(HIDDEN_CLASSNAME);  //hidden 이라는 클래스를 줘서 submit이 되고나면 화면이 사라지게 하기위해 클래스를 줘서 실행해준다.
+  
    const username = loginInput.value; 
-   console.log(username);
+   
+  // greeting.innerText = "Hello! "+ username;  // string을 합치는것!! 아래의 방법을 선호한다.
+   greeting.innerText = `Hello! ${username}`; 
+   greeting.classList.remove(HIDDEN_CLASSNAME);
 } 
 
 //loginButton.addEventListener("click", onLoginBtnClick)
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-function handleLinkClick(event){
-   event.preventDefault();
-   console.log(event);
-   alert("clicked")  // 기본적으로 자바스크립트의 alert는 기본동작을 잠시 막는 역할을 한다. ok누르면 다시 실행됨!!!
+
+
+//a태그 연습한 스크립트
+// function handleLinkClick(event){
+//    event.preventDefault(); 
+//    console.log(event);
+//    alert("clicked")  // 기본적으로 자바스크립트의 alert는 기본동작을 잠시 막는 역할을 한다. ok누르면 다시 실행됨!!!
    
-}
-const link = document.querySelector("a");  //링크찾기
-link.addEventListener("click", handleLinkClick);
+// }
+// const link = document.querySelector("a");  //링크찾기
+// link.addEventListener("click", handleLinkClick);
