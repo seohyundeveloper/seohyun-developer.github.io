2e4c1fcae7819c1ad675488ca6bd38e7517b3e8d
MIN SEO HYUN <seoandjisister77@gmail.com>
2022-06-15 01:31:05 +0900

    함수 조건걸기

--- Javascript/HTML-practice/html/Javascript/app2.js
+++ Javascript/HTML-practice/html/Javascript/app2.js
@@ -8,7 +8,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";  //클래스 이름
-
+const USERNAME_KEY = "username";   //입력된값의 이름
 // function onLoginBtnClick(){
 //    // console.dir(loginInput);
 //   // console.log(loginInput.value);    //input에 입력되는 value 값을 가져올수 있다.
@@ -27,23 +27,32 @@ function onLoginSubmit(event){
    loginForm.classList.add(HIDDEN_CLASSNAME);  //hidden 이라는 클래스를 줘서 submit이 되고나면 화면이 사라지게 하기위해 클래스를 줘서 실행해준다.
   
    const username = loginInput.value; 
-   localStorage.setItem("username", username); // 로컬 스토리지에 저장하기
+   localStorage.setItem( USERNAME_KEY, username); // 로컬 스토리지에 저장하기
   // greeting.innerText = "Hello! "+ username;  // string을 합치는것!! 아래의 방법을 선호한다.
-   greeting.innerText = `Hello! ${username}`; 
-   greeting.classList.remove(HIDDEN_CLASSNAME);
+   // greeting.innerText = `Hello! ${username}`; 
+   // greeting.classList.remove(HIDDEN_CLASSNAME);
+   paintGreetings(username); //함수로 빼서 처리해준다.
 } 
 
 //loginButton.addEventListener("click", onLoginBtnClick)
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username){
+   greeting.innerText = `Hello! ${username}`; 
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-const saveUsername = localStorage.getItem("username");  //로컬스토리지에 저장된 값 가져오기!!!
-//console.log(saveUsername)
+const saveUsername = localStorage.getItem( USERNAME_KEY );  //로컬스토리지에 저장된 값 가져오기!!!
+console.log(saveUsername)
 
-if (saveUsername === null) {
+if (saveUsername === null) { //유저 정보가 로컬 스토리지에 없는경우
    //show the form
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit", onLoginSubmit);
 } else{
    //show the greetings
+   // greeting.classList.remove(HIDDEN_CLASSNAME);
+   // greeting.innerText = `Hello ${saveUsername}`;
+   paintGreetings(saveUsername);;  //위의 두줄이 반복되므로 함수로 빼준다.
 }
 
 
