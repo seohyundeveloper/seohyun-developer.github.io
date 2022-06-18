bdf0008875ea0b815624c4f8e785f6a4adb20773
MIN SEO HYUN <seoandjisister77@gmail.com>
2022-06-18 23:15:06 +0900

    to make todo-list

--- nomad/todo.js
+++ nomad/todo.js
@@ -3,6 +3,17 @@ const toDoInput = document.querySelector("#todo-form input");
 //const toDoInput = toDoForm.querySelector("input"); //위와 동일함.
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");  //삭제버튼 생성
+    button.innerText = "X";
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function hadleToDoSubmit(event){
     event.preventDefault();
     console.log(toDoInput.value)
@@ -10,6 +21,7 @@ function hadleToDoSubmit(event){
     toDoInput.value = ''; //입력하고 엔터 치면 비우도록 하기 
     console.log(newTodo, toDoInput.value)  //여기서 위의 toDoInput.value와는 다르게 찍힌다!! 
     // 8라인의 toDoInput 과 newTodo 와는 같이 같다.
+    paintToDo(newTodo); //요소를 만들어서 입력된 todo-list를 담아서 함수를 호출해준다.
 }
 
-toDoForm.addEventListener("submit", hadleToDoSubmit);
+toDoForm.addEventListener("submit", hadleToDoSubmit); 
