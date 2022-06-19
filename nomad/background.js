ac6f11de6db07ac95be83049f3e7bf73a169ebbb
MIN SEO HYUN <seoandjisister77@gmail.com>
2022-06-19 17:12:51 +0900

    css마무리

--- nomad/background.js
+++ nomad/background.js
@@ -1,8 +1,9 @@
 const images = [ "0.jpg", "1.jpg","2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)]
-console.log(chosenImage, '893487598374598')
+//console.log(chosenImage, '893487598374598')
 const bgImage = document.createElement("img"); //img 태그 만들기
 bgImage.src= `img/${chosenImage}`;
-const container = document.querySelector(".container");
-container.appendChild(bgImage);  // 이미지를 바디에 붙이기
+//const container = document.querySelector(".container");
+document.body.appendChild(bgImage);  // 이미지를 바디에 붙이기
+//container.appendChild(bgImage)
--- nomad/quotes.js
+++ nomad/quotes.js
@@ -1,21 +1,31 @@
 const quotes = [
     {
-        quote:"No bread eaten by man is so sweet as that earned by his own labor.",
-        auther:"seohyun"
+        quote:"I never dreamed about success, I worked for it.",
+        quote_kr:"(나는결코 성공에 대해 꿈꾸지 않는다. 나는 꿈을 위해 행동했다.)",
+        auther:"- Estee Lauder -"
     },
     {
-        quote:"if you do not walk today, you'll have to run tomorrow.",
-        auther:"Anonymous"
+        quote:"Do not try to be original, just try to be good.",
+        quote_kr:"(독특한 사람이 되려하지 말아라. 좋은 사람이 되도록 해라!)",
+        auther:"- Paul Rand -"
     },
     {
-        quote:"I'll prepare, and some day my chance will come.",
-        auther:"Abraham Lincoln"
-    }
+        quote:"Do not be afraid to give up the good to go for the great",
+        quote_kr:"(더 좋은것을 쫓기 위해 좋은 것을 버리는 것을 두려워 하지 마라!)",
+        auther:"- John D.Rockefeller -"
+    },
+    {
+        quote:"Some people dream of success, while other people get up every morning and male it happen.",
+        quote_kr:"(성공을 꿈꾸는 사람들도 있지만, 매일 아침에 일어나 꿈을 실현시키는 사람들도 있다.)",
+        auther:"- Wayne Huizenga -"
+    },
 ]
 
 const quote = document.querySelector("#quote span:first-child");
 const auther = document.querySelector("#quote span:last-child");
-
+const kr = document.querySelector("#quote span:nth-child(2)")
 const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)];
+
 quote.innerText = todaysQuote.quote;
 auther.innerText = todaysQuote.auther;
+kr.innerText = todaysQuote.quote_kr;
